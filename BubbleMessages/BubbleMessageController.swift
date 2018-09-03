04d37beb38ec698b6c576ab46b3cecf78c51f29c
mdeaconu <mihai.deaconu@icloud.com>
2018-09-03 23:21:21 +0300

    Phase 3: custom cell - programmatically

--- BubbleMessages/BubbleMessageController.swift
+++ BubbleMessages/BubbleMessageController.swift
@@ -14,21 +14,18 @@ struct Message {
 
 class BubbleMessageController: UITableViewController {
     
-    fileprivate var messages: [Message] {
-        get {
-            let tmp: [Message] = [
-                Message(label: "This is a long string that should be broken on multiple lines in order to be nicer displayed"),
-                Message(label: "This is a short message")
-            ]
-            return tmp
-        }
-    }
+    fileprivate let messages = [
+        Message(label: "This is a long string that should be broken on multiple lines in order to be nicer displayed"),
+        Message(label: "This is a short message")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.title = "Messages"
-        navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "Messages"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        self.tableView.register(MessageCell.self, forCellReuseIdentifier: "idCell")
     }
 }
 
@@ -39,7 +36,8 @@ extension BubbleMessageController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "idCell", for: indexPath) as! MessageCell
-        cell.setValue(with: messages[indexPath.row].label)
+        cell.messageLabel.text = messages[indexPath.row].label
+//        cell.setValue(with: messages[indexPath.row].label)
         
         return cell
     }
--- BubbleMessages/MessageCell.swift
+++ BubbleMessages/MessageCell.swift
@@ -9,11 +9,64 @@
 import UIKit
 
 class MessageCell: UITableViewCell {
-
-    @IBOutlet weak var messageLabel: UILabel!
+    
+    var messageLabel: UILabel = UILabel()
+    var messageView: UIView = UIView()
+    
+    var leadingConstraint: NSLayoutConstraint!
+    var trailingConstraint: NSLayoutConstraint!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.backgroundColor = .clear
+        self.messageView.backgroundColor = UIColor.green
+        self.messageView.layer.cornerRadius = 15
+        self.messageView.translatesAutoresizingMaskIntoConstraints = false
+        self.messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.messageLabel.numberOfLines = 0
+        
+        self.addSubview(self.messageView)
+        self.addSubview(self.messageLabel)
+        
+        let constraints = [
+            self.messageLabel.topAnchor.constraint(equalTo: topAnchor, constant: 32),
+            self.messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32),
+            self.messageLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 220),
+            
+            self.messageView.topAnchor.constraint(equalTo: self.messageLabel.topAnchor, constant: -16),
+            self.messageView.leadingAnchor.constraint(equalTo: self.messageLabel.leadingAnchor, constant: -16),
+            self.messageView.bottomAnchor.constraint(equalTo: self.messageLabel.bottomAnchor, constant: 16),
+            self.messageView.trailingAnchor.constraint(equalTo: self.messageLabel.trailingAnchor, constant: 16)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+        
+        self.leadingConstraint = self.messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32)
+        self.leadingConstraint.isActive = false
+        
+        self.trailingConstraint = self.messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32)
+        self.trailingConstraint.isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
     public func setValue(with value: String) {
         self.messageLabel.text = value
-        self.messageLabel.numberOfLines = 0
+    }
+}
+
+// MARK: UI Extensions
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
     }
 }
